5dd6525e04eb2cb25b8810f1a97c3375979d7911
Ivo Lejon <ivo.lejon@gmail.com>
2019-06-07 22:33:53 +0200

    added route to get single task by id

--- controllers/task.js
+++ controllers/task.js
@@ -1,25 +1,31 @@
-const Task = require('./../models/task');
+const Task = require("./../models/task");
 const taskController = {
-    create: {
-        async handler(request, h) {
-            const task = await new Task({
-              name: 'ivoTestar',
-              description: 'ivoTestar',
-              status:'active',
-              list:'default list'
-            });
-            task.save();
-            let data = {message: "Task created successfully", task};
-            return h.response(data).code(201);
-        }
-      },
-      all: {
-        async handler(request, h) {
-            let data = await Task.find({}).sort({ createdAt: 'desc' });
-            return h.response(data).code(200);
-        }
-      },
-      
-}
+  create: {
+    async handler(request, h) {
+      const task = await new Task({
+        name: "ivoTestar",
+        description: "ivoTestar",
+        status: "active",
+        list: "default list"
+      });
+      task.save();
+      let data = { message: "Task created successfully", task };
+      return h.response(data).code(201);
+    }
+  },
+  all: {
+    async handler(request, h) {
+      let data = await Task.find({}).sort({ createdAt: "desc" });
+      return h.response(data).code(200);
+    }
+  },
+  get: {
+    async handler(request, h) {
+      const id = request.params.id;
+      let data = await Task.findOne({ _id: id });
+      return h.response(data).code(200);
+    }
+  }
+};
 
 module.exports = taskController;
--- routes/task.js
+++ routes/task.js
@@ -16,15 +16,24 @@ const routes = [
     }
   },
   {
+    // Get all tasks
     method: "GET",
     path: "/api/v1/tasks",
     handler: api.task.all.handler
   },
   {
+    //Get single task
+    method: "GET",
+    path: "/api/v1/task/{id}",
+    handler: api.task.get.handler
+  },
+  {
+    // Create a new task
     method: "POST",
     path: "/api/v1/tasks",
     handler: api.task.create.handler
   }
+
 ];
 
 module.exports = routes;
